09d17fec16ef8792a2bf8a62644a90da56c10caf
MaxRybetsky <stsdewar@ya.ru>
2021-02-22 19:27:30 +0300

    Fix bugs in UserServiceTest

--- src/test/java/rybetsky/bosslang/service/UserServiceTest.java
+++ src/test/java/rybetsky/bosslang/service/UserServiceTest.java
@@ -9,9 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import rybetsky.bosslang.dao.UserDao;
 import rybetsky.bosslang.domain.UserEntity;
+
+import java.util.Optional;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -29,17 +34,22 @@ public class UserServiceTest {
     public void findByUpdateWhenUpdateHasCallbackQuery() {
         UserEntity user = new UserEntity();
         user.setChatId(1L);
-        Mockito.doReturn(user)
+        Optional<UserEntity> userOptional = Optional.of(user);
+        Mockito.doReturn(userOptional)
                 .when(userDao)
                 .findById(1L);
         Mockito.doReturn(true)
                 .when(update)
                 .hasCallbackQuery();
-        Mockito.doReturn(1L)
+        CallbackQuery callbackQuery = new CallbackQuery();
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(1L);
+        message.setChat(chat);
+        callbackQuery.setMessage(message);
+        Mockito.doReturn(callbackQuery)
                 .when(update)
-                .getCallbackQuery()
-                .getMessage()
-                .getChatId();
+                .getCallbackQuery();
         UserEntity userEntity = userService.findByUpdateOrCreateNew(update);
         Assert.assertNotNull(userEntity);
     }
